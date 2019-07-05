ac3cf0d9f278e3a6da1c055dc03492d1ed6b4a6c
Sahil-Bhatia <31650286+Sahil-Bhatia@users.noreply.github.com>
2019-07-05 18:44:30 +0530

    Add files via upload

---
+++ Demo 2/src/app/AddProductComponent.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'add-prod',
+    templateUrl:'app.addproduct.html'
+})
+
+
+export class AddProductComponent{
+
+}
---
+++ Demo 2/src/app/ShowProductComponent.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'show-prod',
+    templateUrl:'app.showproduct.html'
+})
+
+export class ShowProductComponent{
+
+}
