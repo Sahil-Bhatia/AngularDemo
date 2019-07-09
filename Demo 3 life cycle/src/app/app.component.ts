7e962107f3570879a6875ae3d230268b7ed87b11
Sahil-Bhatia <31650286+Sahil-Bhatia@users.noreply.github.com>
2019-07-09 19:13:49 +0530

    Add files via upload

---
+++ Demo 3 life cycle/src/app/app.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component, OnInit,OnDestroy } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: 'app.component.html'
+})
+
+export class AppComponent { //component life cycle
+    constructor(){
+        console.log("In Constructor");
+    }
+    ngOnInit(){
+        console.log("In NgOnInit");
+
+    }
+
+    ngOnDestroy(){ //this will be called when another component is called from this main component
+        console.log("In NgOnDestroy");
+    }
+}
